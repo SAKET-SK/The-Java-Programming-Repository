df8a6edb4d5041624ce950f9a4027920bf68a6c6
Saket Khopkar <saketkhopkar910@gmail.com>
2022-10-15 19:47:45 +0530

    Update DigitalSum.java

--- DigitalSum.java
+++ DigitalSum.java
@@ -6,14 +6,14 @@ class DigitalSum
 	public static void main(String args[])
 	{
 		int num,sum=0,rem;
-		System.out.print("Enter a number: ");
+		System.out.print("Enter a number: ");   // For example, let the number be 712, expected output, 11
 		Scanner sc=new Scanner(System.in);
-		num=sc.nextInt();
+		num=sc.nextInt();      // num = 712
 		while(num>0)
 		{
-			rem=num%10;
-			sum=sum+rem;
-			num=num/10;
+			rem=num%10;   // rem = 712 % 10 = 2
+			sum=sum+rem;  // initial sum = 0, 0+2 = 2
+			num=num/10;   // num = 712 / 10 = 71, 71 greter than 0, so continue with num = 71
 		}
 		System.out.println("Digital Sum: "+sum);
 	}
