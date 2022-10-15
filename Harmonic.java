4207fb64a156b6903c6898a9b7093969ab2f1750
Saket Khopkar <saketkhopkar910@gmail.com>
2022-10-15 12:02:08 +0530

    Create Harmonic.java

---
+++ Harmonic.java
@@ -0,0 +1,15 @@
+// Java Program to print Harmonic Series : 2 6 12 20 30
+
+import java.util.*;
+class Harmonic
+{
+    public static void main(String args[])
+    {
+        int i;
+        Scanner sc = new Scanner(System.in);
+        for(i = 1; i < 5; i++)
+        {
+            System.out.print(i * i + i);
+        }
+    }
+}
