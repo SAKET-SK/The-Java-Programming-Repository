617cc8ea6befdc7d52eb784cee8f1c15cd7c24ba
Saket Khopkar <saketkhopkar910@gmail.com>
2023-01-15 13:27:31 +0530

    Create InstanceOfTest.java

---
+++ InstanceOfTest.java
@@ -0,0 +1,14 @@
+// Java program to demonstrate working of
+// instanceof operator
+  
+public class InstanceOfTest
+{
+    public static void main(String[] args)
+    {
+        Integer i = new Integer(5);
+  
+        // prints true as i is instance of class
+        // Integer
+        System.out.println(i instanceof Integer);
+    }
+}
