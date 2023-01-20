af6212bd8657cac4bee4c3614fa76182c4261e56
Saket Khopkar <saketkhopkar910@gmail.com>
2023-01-20 15:29:06 +0530

    Create RegexExample1.java

---
+++ RegexExample1.java
@@ -0,0 +1,17 @@
+// 3 ways to write regex in java
+import java.util.regex.*;  
+public class RegexExample1{  
+public static void main(String args[]){  
+//1st way  
+Pattern p = Pattern.compile(".s");//. represents single character  
+Matcher m = p.matcher("as");  
+boolean b = m.matches();  
+  
+//2nd way  
+boolean b2=Pattern.compile(".s").matcher("as").matches();  
+  
+//3rd way  
+boolean b3 = Pattern.matches(".s", "as");  
+  
+System.out.println(b+" "+b2+" "+b3);  
+}}  
