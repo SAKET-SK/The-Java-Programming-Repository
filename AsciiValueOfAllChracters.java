eb931ba8f4d2d3b52e9e7715534f54dcdd2726d1
Saket Khopkar <saketkhopkar910@gmail.com>
2023-01-26 10:59:38 +0530

    Create AsciiValueOfAllChracters.java

---
+++ AsciiValueOfAllChracters.java
@@ -0,0 +1,11 @@
+// Printing ASCII value of all characters
+public class AsciiValueOfAllChracters  
+{  
+public static void main(String[] args)   
+{  
+for(int i = 0; i <= 255; i++)  
+{  
+System.out.println(" The ASCII value of " + (char)i + "  =  " + i);  
+}  
+}  
+}   
