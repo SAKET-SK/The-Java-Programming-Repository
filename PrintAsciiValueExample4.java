067ef122fc8e4086214b2dbe259ccc214622fe52
Saket Khopkar <saketkhopkar910@gmail.com>
2023-01-26 10:56:07 +0530

    Create PrintAsciiValueExample4.java

---
+++ PrintAsciiValueExample4.java
@@ -0,0 +1,13 @@
+// Print ASCII Value of any entered character
+import java.util.Scanner;  
+public class PrintAsciiValueExample4  
+{  
+public static void main(String args[])  
+{  
+System.out.print("Enter a character: ");  
+Scanner sc = new Scanner(System.in);  
+char chr = sc.next().charAt(0);  
+int asciiValue = chr;  
+System.out.println("ASCII value of " +chr+ " is: "+asciiValue);  
+}  
+}  
