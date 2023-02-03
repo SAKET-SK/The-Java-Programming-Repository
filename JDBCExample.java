830f3ceda5194035ccc12d2c33be986af2cbe536
Saket Khopkar <saketkhopkar910@gmail.com>
2023-02-03 19:11:59 +0530

    Create JDBCExample.java

---
+++ JDBCExample.java
@@ -0,0 +1,24 @@
+// Creating a Database using JDBC
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCExample {
+   static final String DB_URL = "jdbc:mysql://localhost/";
+   static final String USER = "guest";
+   static final String PASS = "guest123";
+
+   public static void main(String[] args) {
+      // Open a connection
+      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+         Statement stmt = conn.createStatement();
+      ) {		      
+         String sql = "CREATE DATABASE STUDENTS";
+         stmt.executeUpdate(sql);
+         System.out.println("Database created successfully...");   	  
+      } catch (SQLException e) {
+         e.printStackTrace();
+      } 
+   }
+}
