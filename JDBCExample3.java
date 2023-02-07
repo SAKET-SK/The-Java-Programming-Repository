ce1ec51e940c05499eee4d43e4488e5b50c231c2
Saket Khopkar <saketkhopkar910@gmail.com>
2023-02-07 17:28:30 +0530

    Create JDBCExample3.java

---
+++ JDBCExample3.java
@@ -0,0 +1,30 @@
+// Demonstrating Create Table function in Java JDBC
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCExample3 {
+   static final String DB_URL = "jdbc:mysql://localhost/TUTORIALSPOINT";
+   static final String USER = "guest";
+   static final String PASS = "guest123";
+
+   public static void main(String[] args) {
+      // Open a connection
+      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+         Statement stmt = conn.createStatement();
+      ) {		      
+          String sql = "CREATE TABLE REGISTRATION " +
+                   "(id INTEGER not NULL, " +
+                   " first VARCHAR(255), " + 
+                   " last VARCHAR(255), " + 
+                   " age INTEGER, " + 
+                   " PRIMARY KEY ( id ))"; 
+
+         stmt.executeUpdate(sql);
+         System.out.println("Created table in given database...");  
+         } catch (SQLException e) {
+         e.printStackTrace();
+      } 
+   }
+}
